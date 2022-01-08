3cd02f0fe04724fce8ae1fea511d7d748e39d59e
vic-shihang-li <viclee@nyu.edu>
2022-01-07 23:16:58 -0500

    build: add util to check if module is affected

---
+++ scripts/nx-affected.js
@@ -0,0 +1,23 @@
+const execSync = require("child_process").execSync;
+
+/**
+ * Tests whether a NX project (app or lib) has been affected.
+ *
+ * @param {string} projectName
+ * @param {string} baseCommit
+ *
+ * @returns a boolean indicating whether this project has been affected.
+ */
+function isProjectAffected(projectName, baseCommit, headCommit) {
+  headCommit = headCommit ? headCommit : "HEAD";
+  let getAffected = baseCommit
+    ? `nx print-affected --base=${baseCommit} --head=${headCommit}`
+    : `nx print-affected`;
+
+  const output = execSync(getAffected).toString();
+  const changedProjects = JSON.parse(output).projects;
+
+  return changedProjects.includes(projectName);
+}
+
+module.exports = isProjectAffected;
---
+++ scripts/nx-affected.spec.js
@@ -0,0 +1,38 @@
+/**
+ * Simple test file that tests nx-affected.js.
+ *
+ * This test file is not discoverable by the nx test runner. To test, run:
+ * `node nx-affected.spec.js`
+ */
+
+const isProjectAffected = require("./nx-affected");
+
+const HEAD_COMMIT = "b5689e7c89873b3a74a5129fd087c4fb5e10a6d8";
+
+function assertTrue(value) {
+  if (value !== true) {
+    throw new Error(`Expected 'true', got ${value}`);
+  }
+}
+
+function testReturnTrueIfProjectChanged() {
+  const OLD_COMMIT = "c70077323ee2d57e8f0826d49fef2fc467562543";
+  assertTrue(isProjectAffected("todo-app", OLD_COMMIT, HEAD_COMMIT));
+}
+
+function testReturnFalseIfProjectDidNotChange() {
+  const OLD_COMMIT = "b5689e7c89873b3a74a5129fd087c4fb5e10a6d8";
+  assertTrue(!isProjectAffected("todo-app", OLD_COMMIT, HEAD_COMMIT));
+}
+
+function testReturnFalseForNonexistentProject() {
+  assertTrue(!isProjectAffected("bogus-project", null, HEAD_COMMIT));
+}
+
+function test() {
+  testReturnTrueIfProjectChanged();
+  testReturnFalseIfProjectDidNotChange();
+  testReturnFalseForNonexistentProject();
+}
+
+test();
