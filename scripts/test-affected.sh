e8a4ab847f49c738287460747ccd0f001884faae
vic-shihang-li <viclee@nyu.edu>
2022-01-13 21:20:46 -0500

    ci: store if electron is affected in a bash var

--- scripts/test-affected.sh
+++ scripts/test-affected.sh
@@ -5,11 +5,11 @@ USAGE="usage: ./test-affected.sh [nx-project-name] <base-commit> <head-commit>"
 if [ "$#" -eq 0 ]; then
     echo $USAGE
 elif [ "$#" -eq 1 ]; then
-	node ./scripts/nx-affected.js $1 | grep 'true'
+	node ./scripts/nx-affected.js $1 | grep 'true' && echo 'true' || echo 'false'
 elif [ "$#" -eq 2 ]; then
-	node ./scripts/nx-affected.js $1 $2 | grep 'true'
+	node ./scripts/nx-affected.js $1 $2 | grep 'true' && echo 'true' || echo 'false'
 elif [ "$#" -eq 3 ]; then
-	node ./scripts/nx-affected.js $1 $2 $3 | grep 'true'
+	node ./scripts/nx-affected.js $1 $2 $3 | grep 'true' && echo 'true' || echo 'false'
 else
 	echo $USAGE
 fi
