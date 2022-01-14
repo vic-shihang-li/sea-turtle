4ed0440ee7d1c07782891555dd390b7974566170
vic-shihang-li <viclee@nyu.edu>
2022-01-13 21:01:06 -0500

    ci: ignore output from grep

--- scripts/test-affected.sh
+++ scripts/test-affected.sh
@@ -5,11 +5,11 @@ USAGE="usage: ./test-affected.sh [nx-project-name] <base-commit> <head-commit>"
 if [ "$#" -eq 0 ]; then
     echo $USAGE
 elif [ "$#" -eq 1 ]; then
-	node ./scripts/nx-affected.js $1 | grep 'true'
+	node ./scripts/nx-affected.js $1 | grep 'true' &> /dev/null
 elif [ "$#" -eq 2 ]; then
-	node ./scripts/nx-affected.js $1 $2 | grep 'true'
+	node ./scripts/nx-affected.js $1 $2 | grep 'true' &> /dev/null
 elif [ "$#" -eq 3 ]; then
-	node ./scripts/nx-affected.js $1 $2 $3 | grep 'true'
+	node ./scripts/nx-affected.js $1 $2 $3 | grep 'true' &> /dev/null
 else
 	echo $USAGE
 fi
