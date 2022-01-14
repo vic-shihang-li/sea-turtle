6401fc2098149e086844d003fd3146636c828b0a
vic-shihang-li <viclee@nyu.edu>
2022-01-13 21:05:05 -0500

    Revert "ci: ignore output from grep"
    This reverts commit 4ed0440ee7d1c07782891555dd390b7974566170.

--- scripts/test-affected.sh
+++ scripts/test-affected.sh
@@ -5,11 +5,11 @@ USAGE="usage: ./test-affected.sh [nx-project-name] <base-commit> <head-commit>"
 if [ "$#" -eq 0 ]; then
     echo $USAGE
 elif [ "$#" -eq 1 ]; then
-	node ./scripts/nx-affected.js $1 | grep 'true' &> /dev/null
+	node ./scripts/nx-affected.js $1 | grep 'true'
 elif [ "$#" -eq 2 ]; then
-	node ./scripts/nx-affected.js $1 $2 | grep 'true' &> /dev/null
+	node ./scripts/nx-affected.js $1 $2 | grep 'true'
 elif [ "$#" -eq 3 ]; then
-	node ./scripts/nx-affected.js $1 $2 $3 | grep 'true' &> /dev/null
+	node ./scripts/nx-affected.js $1 $2 $3 | grep 'true'
 else
 	echo $USAGE
 fi
